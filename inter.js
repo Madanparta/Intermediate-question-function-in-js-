2cd0094a7cc71acb5481e2f83129b45a264f83bb
madan k <partagowda15@gmil.com>
2022-07-02 09:42:31 +0530

    intermediate questions

---
+++ inter.js
@@ -0,0 +1,56 @@
+// 1. 
+
+// function areaOfRectangle (lenght, bradth){
+//     let area = lenght*bradth
+//     return area
+// }
+// console.log(areaOfRectangle(7,3))
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 2.
+
+// function perimeterOfRectangle (lenght,bradth){
+//     let perimeter = 2*(lenght+bradth)
+//     let rectangle = lenght*bradth
+//     let sumOf = perimeter+rectangle
+//     return sumOf
+// }
+// console.log(perimeterOfRectangle(10,2))
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 3.
+
+// function numberOfDay (days){
+//     let year = Math.floor(days/365)
+//     let month = Math.floor((days-(year*365))/30)
+//     let day = (days-((year*365)+(month*30)))
+//     return ("Years ="+year+" , Months =" +month+" , Days = "+ day)
+// }
+// console.log(numberOfDay(35))
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 4.
+
+// function hourMinutSeco (seconds){
+//     let hr = Math.floor(seconds/3600)
+//     let min = Math.floor((seconds-(hr*3600))/60)
+//     let sec = (seconds-((hr*3600)+(min*60)))
+//     return ("Hours = "+hr+", Minutes = "+min+", Seconds = "+sec)
+// }
+// console.log(hourMinutSeco(3752))
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 5.
+
+// function squareCube (num){
+//     let square = num**2
+//     let cube = num**3
+//     let sumOf = (square+cube)
+//     return sumOf
+// }
+// console.log(squareCube(25))
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Intermediate questions.
+
+`1.` Area of a Rectangle
+Given length and breadth of a rectangle return the area of a rectangle.
+
+---
+
+`2.`Sum of area and perimeter of a rectangle
+
+Given length and breadth of a rectangle return the sum of area and perimeter of the rectangle.
+
+---
+
+`3.`Number of Days
+
+Given years,months and days as input, return the number of days as output.
+
+---
+
+`4.`Number of Seconds
+
+Given the number of hours,minutes and seconds, return the number of seconds.
+
+---
+
+`5.`Sum of Square and Cube of a Number
+
+Given a number return its sum of square and cube.
